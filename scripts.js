e7b5b16f183980cb8853d9f394dfa0c14e7bf273
lucasfsf <lucasfreireri@gmail.com>
2023-01-02 07:35:39 -0300

    Create single box and add it to container div

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 // Get reference to container
 const container = document.getElementById('container');
 
+// Create single square
+let square = document.createElement('div');
+square.classList.add('square');
+
+// Append Square to Container
+container.appendChild(square);
