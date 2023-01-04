d7d307434747ce6e353dc02c84c1f561c1ea1c27
lucasfsf <lucasfreireri@gmail.com>
2023-01-04 06:50:35 -0300

    Refactor grid creation to be a function

--- scripts.js
+++ scripts.js
@@ -1,22 +1,25 @@
-let gridSize = 16;
+let defaultGridSize = 16;
 
 // Get reference to container
 const container = document.getElementById('container');
-// Change container grid
-container.style.gridTemplateColumns = `repeat(${gridSize}, calc(800px/${gridSize}) [col-start])`;
-container.style.gridTemplateRows = `repeat(${gridSize}, calc(800px/${gridSize}) [col-start])`;
 
+function createGrid(gridSize) {
+    // Change container grid
+    container.style.gridTemplateColumns = `repeat(${gridSize}, calc(800px/${gridSize}) [col-start])`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, calc(800px/${gridSize}) [col-start])`;
 
-for (let i = 0; i < (gridSize*gridSize); i++) {
-    // Create single square
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.style.height = `calc(800px/${gridSize})`;
-    square.style.width = `calc(800px/${gridSize})`;
-
-    // Append Square to Container
-    container.appendChild(square);
+    for (let i = 0; i < (gridSize*gridSize); i++) {
+        // Create single square
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.style.height = `calc(800px/${gridSize})`;
+        square.style.width = `calc(800px/${gridSize})`;
+        // Append Square to Container
+        container.appendChild(square);
+    }
 }
+
+createGrid(defaultGridSize);
 
 //get all squares
 let squares = document.querySelectorAll('.square');
@@ -25,3 +28,4 @@ squares.forEach(square => square.addEventListener('mouseover', colorSquare));
 function colorSquare(e) {
     this.classList.add('square-colored');
 }
+
